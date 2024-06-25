5d8fb37f3f2970d19a87b2647fa02abca105b3af
Minidu-Sathsara <138673580+Minidu-Sathsara@users.noreply.github.com>
2024-06-25 14:03:34 +0530

    Add x, o

--- app.js
+++ app.js
@@ -2,13 +2,27 @@ let rows = document.querySelectorAll(".row");
 let cells = document.querySelectorAll(".cell");
 let board= Array.from({length:3},()=>Array(3).fill(null));
 
-rows.forEach((row,r)) => ((row.id=r));
-cells.forEach((cell,id)) => ((cell.id=id %3));
+rows.forEach((row,r) => (row.id=r));
+cells.forEach((cell,id) => (cell.id=id %3));
 
 let crossPlaying = true;
 
-cells.forEach((cell)){
-    if (cell.classList.contains("")) {
-        crossPlaying = false;
-    }
-}
+cells.forEach((cell) => {
+    cell.addEventListener("click",()=>{
+        if(!cell.classList.contains("idle"))return;
+
+        let curRow = parseInt(cell.parentElement.id);
+        let curCell= parseInt(cell.id);
+        
+        let currentplayer= crossPlaying ? "x" : "o";
+        let playerClass = crossPlaying ? "cross" : "circle";
+
+        board[curRow][curCell] = currentplayer;
+        cell.classList.add(playerClass);
+        cell.classList.remove("idle");
+
+        crossPlaying = !crossPlaying;
+    });
+    });
+        
+    
